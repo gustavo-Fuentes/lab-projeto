cf0ee4f47daf520e5303780cedc0631bf0ef786c
Jefferson Ferreira <jefferson.bizarri@gmail.com>
2018-04-27 22:01:31 -0300

    Update JavaApplication1.java

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -100,8 +100,8 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Visualizador f = new Visualizador();
+        Engine e = null;
+        Visualizador f = new Visualizador(e);
 
         f.setVisible(true);
     }
