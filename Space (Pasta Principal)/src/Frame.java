f2bb2473669c1f03c0f58eda5da57789e9bfe69e
Jefferson Ferreira <jefferson.bizarri@gmail.com>
2018-05-04 22:24:54 -0300

    Update Frame.java

--- Space (Pasta Principal)/src/Frame.java
+++ Space (Pasta Principal)/src/Frame.java
@@ -3,22 +3,19 @@ import javax.swing.JFrame;
 
 public class Frame extends JFrame{
     Screen s;
+    JFrame f;
     public Frame(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800,600);
-        setResizable(false);
-        setTitle("Projeto");
-        
-        init();
+    	f = new JFrame();
+    	s = new Screen();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(800,600);
+        f.setResizable(false);
+        f.setTitle("Projeto");
+        f.setLocationRelativeTo(null);
+        f.add(s);
+        f.setVisible(true);
     }
-    public void init(){
-        setLocationRelativeTo(null);
-        setLayout(new GridLayout(1, 1, 0, 0));
-        
-        s = new Screen();
-        add(s);
-        setVisible(true);
-    }
+
     public static void main(String[] args) {
      Frame x = new Frame();
     }
